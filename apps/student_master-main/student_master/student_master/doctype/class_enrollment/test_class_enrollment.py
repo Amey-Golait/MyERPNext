697646b9453eb1c283a52866c59e782621b1c2bd
AmeyGolait <152851721+Amey-Golait@users.noreply.github.com>
2025-07-23 18:32:55 +0530

    Delete apps/student_master-main directory

--- apps/student_master-main/student_master/student_master/doctype/class_enrollment/test_class_enrollment.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Amey Golait and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestClassEnrollment(FrappeTestCase):
-	pass
--- apps/student_master-main/student_master/student_master/doctype/guardian_detail/guardian_detail.py
+++
@@ -1,7 +0,0 @@
-# student_master/student_master/doctype/guardian_detail/guardian_detail.py
-
-import frappe
-from frappe.model.document import Document
-
-class GuardianDetail(Document):
-    pass
